c9c808f36b85e4ba4b128c68e4092426cb6004ee
maverick <shiba14148@gmail.com>
2024-05-16 11:57:51 +0900

    セクション7-2まで

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Test;
+
+class TestController extends Controller
+{
+    public function index()
+    {
+        $values = Test::all();
+
+        $count = Test::count();
+
+        $first = Test::findOrFail(1);
+
+        $whereBBB = Test::where('text','=','bbb')->get();
+
+        dd($values, $count, $first, $whereBBB);
+
+        return view('tests.test',compact('values'));
+    }
+}
